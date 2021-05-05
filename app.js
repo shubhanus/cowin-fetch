f245cc71e90b0e5757a24db1400522e5a3739c56
Shubhanu sharma <shubhanus@live.com>
2021-05-05 16:27:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const { default: axios } = require("axios");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const defaultPincode = 313001;
+const defaultAge = 18;
+
+rl.question(`Enter Pincode:(${defaultPincode})`, (pincode) => {
+  rl.question(`Enter age:(${defaultAge})`, (age) => {
+    recursiveGetFreeSlot(
+      Number(pincode) || defaultPincode,
+      Number(age) || defaultAge
+    );
+    rl.close();
+  });
+});
+
+const getDateTodaysDate = () => {
+  const date = new Date();
+  const day = `${date.getDate()}`;
+  const month = `${date.getMonth() + 1}`;
+  return `${day.padStart(2, 0)}-${month.padStart(2, 0)}-${date.getFullYear()}`;
+};
+
+const req = {
+  getCalByPincode: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin`,
+};
+
+const getAvailableSlots = (centers, age) => {
+  const availableCenters = centers.filter((center) => {
+    if (center.fee_type === "Free") {
+      return center.sessions.find(
+        (session) =>
+          session.available_capacity > 0 && session.min_age_limit === age
+      );
+    }
+  });
+
+  return availableCenters;
+};
+
+const getFreeSlot = (pincode, age, intervel) => {
+  const date = getDateTodaysDate();
+  return axios
+    .get(req.getCalByPincode, {
+      params: {
+        pincode,
+        date,
+      },
+    })
+    .then(({ data }) => {
+      const { centers } = data;
+      const availableCenters = getAvailableSlots(centers, age);
+      return availableCenters;
+    });
+};
+
+const sleepTime = 500 * 60;
+// const sleepTime = 1000;
+const getFreeSlotWithFeedback = (pincode, age) => {
+  console.log(`\n\nFinding availablity for ${pincode} and age ${age}.`);
+  return new Promise((resolve, reject) => {
+    getFreeSlot(pincode, age).then((availableCenters) => {
+      if (availableCenters && availableCenters.length) {
+        console.log("\n\n\ncenters found:", availableCenters);
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  });
+};
+
+const recursiveGetFreeSlot = (pincode, age) => {
+  getFreeSlotWithFeedback(pincode, age).catch(() => {
+    const timeout = setInterval(async () => {
+      try {
+        await getFreeSlotWithFeedback(pincode, age);
+        clearTimeout(timeout);
+        console.log("\n\nbye bye");
+      } catch (error) {
+        console.log(
+          `Sleeping for ${sleepTime / 1000 / 60} mins before next attempt.`
+        );
+      }
+    }, sleepTime);
+  });
+};
